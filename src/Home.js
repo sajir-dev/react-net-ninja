ce83866d80b7ac48081a0826e0897beff00bf364
sajir-dev <sajirt@gmail.com>
2021-06-04 12:18:50 +0530

    15-useEffect dependency

--- src/Home.js
+++ src/Home.js
@@ -8,20 +8,27 @@ const Homepage = () => {
         {title:"Home brewery", body:"Story of a few drunkards", author:"Martin", id:3}
     ])
 
+    const [name, setName] = useState("mario")
+
     const handleDelete = (id) => {
         let newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs)
     }
 
+    const changeName = (newName) => {
+        setName(newName)
+    }
+
     useEffect(() => {
         console.log("useEffect ran")
-        console.log(blogs)
-    })
+    }, [name])
 
     return (
         <div className="homepage">
             <BlogsList blogs = { blogs } title = "All Blogs" handleDelete = { handleDelete }/>
             <BlogsList blogs = { blogs.filter(blog => blog.author === "Martin") } title = "Martin's Blogs" handleDelete = { handleDelete }/>
+            <button onClick = {() => changeName("puzo")}>change name</button>
+            <p>{ name }</p>
         </div>
     )
 }
