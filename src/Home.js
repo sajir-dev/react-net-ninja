4256537bd2d72647f620d378fa21dff23d2cd8ac
sajir-dev <sajirt@gmail.com>
2021-06-04 15:21:24 +0530

    19-loading and error handling

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,44 @@ import BlogsList from './BlogsList'
 
 const Homepage = () => {
     const [blogs, setBlogs] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [isErr, setIsError] = useState(false)
 
     useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-        .then(res => {
-            return res.json()
-        })
-        .then(
-            data => setBlogs(data)
-        )
-    }, [])
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
+            .then(
+                res => {
+                    // console.log(res)
+                    if (!res.ok) {
+                        setIsError(true)
+                        setIsLoading(false)
+                        throw Error('could not catch data for the resource');
+                    }
+                    return res.json()
+                })
+            .then(
+                data => {
+                    setBlogs(data)
+                    setIsLoading(false)
+                    setIsError(false)
+                }
+            )
+            .catch(
+                err => {
+                    console.log(err)
+                    setIsError(true)
+                    setIsLoading(false)
+                }
+            )
+        }, 1000)
+        }, [])
+
 
     return (
         <div className="homepage">
+            { isLoading && <p>Loading...</p>}
+            { isErr && <p>Could not fetch resources</p>}
             { blogs && <BlogsList blogs = { blogs } title = "All Blogs"/> }
         </div>
     )
